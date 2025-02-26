1842531aa7d6a8fa38fe67691290432ea40a4ab8
Chaitanya Jawanjal <chaitanya jawanjal>
2025-02-26 10:09:59 +0530

    implemented search feature in dashboard screen

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -11,6 +11,21 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   List<dynamic> customers = [];
   bool _isLoading = true;
+  TextEditingController _searchController = TextEditingController();
+  List<dynamic> filteredCustomers = [];
+  void _filterCustomers(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        filteredCustomers = List.from(customers); // Reset list when empty
+      } else {
+        filteredCustomers =
+            customers.where((customer) {
+              final name = customer['name']?.toLowerCase() ?? '';
+              return name.contains(query.toLowerCase());
+            }).toList();
+      }
+    });
+  }
 
   @override
   void initState() {
@@ -26,7 +41,10 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
-        setState(() => customers = data['customers'] ?? []);
+        setState(() {
+          customers = data['customers'] ?? [];
+          filteredCustomers = List.from(customers); // Initialize filtered list
+        });
       }
     } catch (e) {
       print("Error fetching data: $e");
@@ -329,143 +347,175 @@ class _DashboardScreenState extends State<DashboardScreen> {
               ? Center(child: CircularProgressIndicator())
               : RefreshIndicator(
                 onRefresh: _fetchCustomerData,
-                child:
-                    customers.isEmpty
-                        ? Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.people_outline,
-                                size: 80,
-                                color: Colors.grey,
-                              ),
-                              const SizedBox(height: 10),
-                              const Text(
-                                "No customers found",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
+                child: Column(
+                  children: [
+                    // Search Bar
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextField(
+                        controller: _searchController,
+                        decoration: InputDecoration(
+                          labelText: 'Search by Name',
+                          prefixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                        )
-                        : ListView.builder(
-                          padding: EdgeInsets.all(16),
-                          itemCount: customers.length,
-                          itemBuilder: (context, index) {
-                            final customer = customers[index];
-                            return Card(
-                              margin: const EdgeInsets.symmetric(
-                                horizontal: 16,
-                                vertical: 8,
-                              ),
-                              elevation: 4,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(16),
+                        ),
+                        onChanged:
+                            _filterCustomers, // Calls the function on typing
+                      ),
+                    ),
+
+                    // Customers List
+                    Expanded(
+                      child:
+                          filteredCustomers.isEmpty
+                              ? Center(
                                 child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
-                                    Row(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        CircleAvatar(
-                                          backgroundImage:
-                                              customer['customerImage']?['url'] !=
-                                                      null
-                                                  ? NetworkImage(
-                                                    customer['customerImage']['url'],
-                                                  )
-                                                  : null,
-                                          backgroundColor: Colors.grey[300],
-                                          radius: 30,
-                                          child:
-                                              customer['customerImage']?['url'] ==
-                                                      null
-                                                  ? const Icon(
-                                                    Icons.person,
-                                                    color: Colors.white,
-                                                    size: 30,
-                                                  )
-                                                  : null,
-                                        ),
-                                        const SizedBox(width: 16),
-                                        Expanded(
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                customer['name'] ?? 'Unknown',
-                                                style: const TextStyle(
-                                                  fontSize: 18,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                                overflow: TextOverflow.ellipsis,
-                                              ),
-                                              const SizedBox(height: 8),
-                                              Text(
-                                                "Fees Paid: ₹${customer['feesPaid'] ?? 0}",
-                                                style: TextStyle(
-                                                  fontSize: 14,
-                                                  fontWeight: FontWeight.w500,
-                                                  color: Colors.green,
-                                                ),
-                                              ),
-                                              const SizedBox(height: 8),
-                                              Text(
-                                                "Fees Remaining: ₹${2300 - (customer['feesPaid'] ?? 0)}",
-                                                style: TextStyle(
-                                                  fontSize: 14,
-                                                  fontWeight: FontWeight.w500,
-                                                ),
-                                              ),
-                                              const SizedBox(height: 8),
-                                              Text(
-                                                "Start Date: ${formatTimestamp(customer['start_date'])}",
-                                                style: TextStyle(
-                                                  fontSize: 14,
-                                                  color: Colors.blueGrey,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        PopupMenuButton<String>(
-                                          onSelected:
-                                              (value) => _onMenuItemSelected(
-                                                value,
-                                                customer,
-                                              ),
-                                          itemBuilder:
-                                              (context) => [
-                                                PopupMenuItem(
-                                                  value: 'manage',
-                                                  child: Text('Manage'),
-                                                ),
-                                                PopupMenuItem(
-                                                  value: 'renew',
-                                                  child: Text('Renew'),
-                                                ),
-                                                PopupMenuItem(
-                                                  value: 'delete',
-                                                  child: Text('Delete'),
-                                                ),
-                                              ],
-                                        ),
-                                      ],
+                                    Icon(
+                                      Icons.people_outline,
+                                      size: 80,
+                                      color: Colors.grey,
+                                    ),
+                                    const SizedBox(height: 10),
+                                    const Text(
+                                      "No customers found",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.grey,
+                                      ),
                                     ),
                                   ],
                                 ),
+                              )
+                              : ListView.builder(
+                                padding: EdgeInsets.all(16),
+                                itemCount: filteredCustomers.length,
+                                itemBuilder: (context, index) {
+                                  final customer = filteredCustomers[index];
+                                  return Card(
+                                    margin: const EdgeInsets.symmetric(
+                                      horizontal: 16,
+                                      vertical: 8,
+                                    ),
+                                    elevation: 4,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(16),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              CircleAvatar(
+                                                backgroundImage:
+                                                    customer['customerImage']?['url'] !=
+                                                            null
+                                                        ? NetworkImage(
+                                                          customer['customerImage']['url'],
+                                                        )
+                                                        : null,
+                                                backgroundColor:
+                                                    Colors.grey[300],
+                                                radius: 30,
+                                                child:
+                                                    customer['customerImage']?['url'] ==
+                                                            null
+                                                        ? const Icon(
+                                                          Icons.person,
+                                                          color: Colors.white,
+                                                          size: 30,
+                                                        )
+                                                        : null,
+                                              ),
+                                              const SizedBox(width: 16),
+                                              Expanded(
+                                                child: Column(
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  children: [
+                                                    Text(
+                                                      customer['name'] ??
+                                                          'Unknown',
+                                                      style: const TextStyle(
+                                                        fontSize: 18,
+                                                        fontWeight:
+                                                            FontWeight.bold,
+                                                      ),
+                                                      overflow:
+                                                          TextOverflow.ellipsis,
+                                                    ),
+                                                    const SizedBox(height: 8),
+                                                    Text(
+                                                      "Fees Paid: ₹${customer['feesPaid'] ?? 0}",
+                                                      style: TextStyle(
+                                                        fontSize: 14,
+                                                        fontWeight:
+                                                            FontWeight.w500,
+                                                        color: Colors.green,
+                                                      ),
+                                                    ),
+                                                    const SizedBox(height: 8),
+                                                    Text(
+                                                      "Fees Remaining: ₹${2300 - (customer['feesPaid'] ?? 0)}",
+                                                      style: TextStyle(
+                                                        fontSize: 14,
+                                                        fontWeight:
+                                                            FontWeight.w500,
+                                                      ),
+                                                    ),
+                                                    const SizedBox(height: 8),
+                                                    Text(
+                                                      "Start Date: ${formatTimestamp(customer['start_date'])}",
+                                                      style: TextStyle(
+                                                        fontSize: 14,
+                                                        color: Colors.blueGrey,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                              PopupMenuButton<String>(
+                                                onSelected:
+                                                    (value) =>
+                                                        _onMenuItemSelected(
+                                                          value,
+                                                          customer,
+                                                        ),
+                                                itemBuilder:
+                                                    (context) => [
+                                                      PopupMenuItem(
+                                                        value: 'manage',
+                                                        child: Text('Manage'),
+                                                      ),
+                                                      PopupMenuItem(
+                                                        value: 'renew',
+                                                        child: Text('Renew'),
+                                                      ),
+                                                      PopupMenuItem(
+                                                        value: 'delete',
+                                                        child: Text('Delete'),
+                                                      ),
+                                                    ],
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },
                               ),
-                            );
-                          },
-                        ),
+                    ),
+                  ],
+                ),
               ),
     );
   }
